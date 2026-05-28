9916afe203c245ee7cd2e2fb1f5b0596e80961e3
Mikey <nexasstudiosdevelopment@gmail.com>
2026-05-28 11:52:07 +0200

    updated version number and documentation

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  base: '/MagiskNext/',
+  base: '/',
   title: "Magisk Next",
   description: "The Next Generation of Root",
   ignoreDeadLinks: true,
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Guide', link: '/guide/' },
-      { text: 'Developers', link: '/developers/' }
+      { text: 'Install', link: '/install' },
+      { text: 'OTA', link: '/ota' },
+      { text: 'Developers', link: '/developers/' },
+      { text: 'Changelog', link: '/changes' }
     ],
 
     sidebar: [
@@ -17,16 +19,26 @@ export default defineConfig({
         text: 'Getting Started',
         items: [
           { text: 'Introduction', link: '/guide/' },
-          { text: 'Installation', link: '/guide/installation' }
+          { text: 'Installation', link: '/install' },
+          { text: 'OTA Survival', link: '/ota' },
+          { text: 'FAQ', link: '/faq' }
+        ]
+      },
+      {
+        text: 'User Docs',
+        items: [
+          { text: 'Guides', link: '/guides' },
+          { text: 'Tools', link: '/tools' },
+          { text: 'Boot Details', link: '/boot' },
+          { text: 'App Changes', link: '/app_changes' }
         ]
       },
       {
         text: 'Module Developers',
         items: [
           { text: 'Module Hub Integration', link: '/developers/' },
-          { text: 'WebUI Bridge', link: '/developers/webui' },
-          { text: 'Module Banners', link: '/developers/banners' },
-          { text: 'Action Scripts', link: '/developers/action-scripts' }
+          { text: 'Build & Development', link: '/build' },
+          { text: 'Internal Details', link: '/details' }
         ]
       }
     ],
